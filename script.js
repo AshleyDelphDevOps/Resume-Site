b223a7ebeb0a5c3ddd7347f683ea26bea565c7fa
AshleyDelphDevOps <ashleydelphMTA@outlook.com>
2023-05-31 20:55:46 +0100

    Updating code

--- script.js
+++ script.js
@@ -11,4 +11,26 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  
+    scrollToTopBtn.addEventListener('click', function() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
   
